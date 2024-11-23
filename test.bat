dd91af45f9f425a4ebc90aa5c8de902f5ea619cb
fajitaman6000 <robertmeucci99@gmail.com>
2024-11-23 09:00:34 -0800

    update readme

---
+++ test.bat
@@ -0,0 +1,13 @@
+@echo off
+REM Using start command to run programs asynchronously
+REM /D sets starting directory, /MIN minimizes window if desired
+
+REM Launch admin program on left monitor (primary - 1920,0 assumes 1920x1080 resolution)
+start "" /D "%~dp0" cmd /c "python admin/admin_main.py"
+
+REM Launch kiosk program
+start "" /D "%~dp0" cmd /c "python kiosk/kiosk.py"
+
+REM Alternative version using absolute positioning for first window
+REM Requires NirCmd utility: https://www.nirsoft.net/utils/nircmd.html
+REM start "" /D "%~dp0" cmd /c "nircmd win setpos title "Python" 0 0 960 1080 && python admin/admin_main.py"
