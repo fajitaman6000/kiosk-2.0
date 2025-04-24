f16d74fd6788e04138ad18c05bc1c4ea5d93d1cf
fajitaman6000 <robertmeucci99@gmail.com>
2025-04-24 13:44:02 -0700

    todo

---
+++ admin.bat
@@ -0,0 +1,11 @@
+@echo off
+REM Using start command to run programs asynchronously
+REM /D sets starting directory, /MIN minimizes window if desired
+
+start "" /D "%~dp0" cmd /c "python admin/admin_main.py"
+
+REM Launch kiosk program
+
+REM Alternative version using absolute positioning for first window
+REM Requires NirCmd utility: https://www.nirsoft.net/utils/nircmd.html
+REM start "" /D "%~dp0" cmd /c "nircmd win setpos title "Python" 0 0 960 1080 && python admin/admin_main.py"
